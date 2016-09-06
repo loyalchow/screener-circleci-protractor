d22bad853e48f6afea43d111924a7b6b86ad3790
Loyal Chow <loyalchow@gmail.com>
2016-09-06 12:19:53 -0700

    prepend http to url

--- spec/test-spec.js
+++ spec/test-spec.js
@@ -1,6 +1,6 @@
 describe('Protractor Example', function() {
   it('should take snapshot of homepage', function() {
-    var baseUrl = process.env.CIRCLE_BRANCH + '.127.0.0.1.xip.io:8080';
+    var baseUrl = 'http://' + process.env.CIRCLE_BRANCH + '.127.0.0.1.xip.io:8080';
     browser.driver.get(baseUrl);
     browser.driver.executeScript('/*@screener.snapshot*/', 'Home');
   });
